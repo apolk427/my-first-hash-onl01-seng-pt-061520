bedf12e77c0bf6dacc2bde7df3c3adcd045e6e53
Amyre Polk <myremyre101@icloud.com>
2020-06-25 18:17:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def key_for_min_value(hash)
   lowest_key = hash.reduce { |key, value|key.last > value.last ? value : key}
 
 
-def key_for_min_value(name_hash)	    
+def key_for_min_value(name_hash)
   if hash == {}
       lowest_key
     else
